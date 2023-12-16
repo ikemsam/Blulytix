a76d2648311faff9c98a35471f6830361dcec1ac
Amitkr316 <151126076+Amitkr316@users.noreply.github.com>
2023-12-16 10:38:20 +0530

    Update layout.tsx

--- src/app/(content)/layout.tsx
+++ src/app/(content)/layout.tsx
@@ -28,6 +28,55 @@ export default async function RootLayout({
 <meta name="geo.position" content="54.501178;18.434342" />
 <meta name="ICBM" content="54.501178, 18.434342" />
 
+        <script type="application/ld+json">
+{
+  "@context": "https://schema.org",
+  "@type": "LocalBusiness",
+  "name": "Blulytix",
+  "image": "https://blulytix.ai/_next/image?url=%2Fdata-analysis.png&w=1920&q=75",
+  "@id": "",
+  "url": "https://blulytix.ai/",
+  "telephone": "+48 669 256 146",
+  "priceRange": "$$",
+  "address": {
+    "@type": "PostalAddress",
+    "streetAddress": "grabowskiego, Gdynia, 81-602",
+    "addressLocality": "Gdynia",
+    "postalCode": "81-602",
+    "addressCountry": "PL"
+  } ,
+  "sameAs": [
+    "https://web.facebook.com/blulytixx?_rdc=1&_rdr",
+    "https://twitter.com/Blulytix",
+    "https://www.instagram.com/blulytix/",
+    "https://www.linkedin.com/company/blulytix/?originalSubdomain=pl"
+  ] 
+}
+</script>
+<script type='application/ld+json'> 
+{
+  "@context": "http://www.schema.org",
+  "@type": "Organization",
+  "name": "Blulytix",
+  "url": "https://blulytix.ai/",
+  "logo": "https://blulytix.ai/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogo_dark.f3bbc820.png&w=256&q=75",
+  "image": "https://blulytix.ai/_next/image?url=%2Fdata-analysis.png&w=1920&q=75",
+  "description": "At Blulytix, we are on a passionate quest to harness the power of AI, specializing in UiPath RPA and Python, to propel businesses forward. Rooted in the conviction that AI is a gateway to untapped potential, we focus on delivering cutting-edge solutions in data analysis with Python and digital marketing. Join us on this dynamic journey, where innovation and commitment converge to empower businesses for success in today's fast-paced landscape.",
+  "address": {
+    "@type": "PostalAddress",
+    "streetAddress": "grabowskiego, Gdynia, 81-602",
+    "addressLocality": "Gdynia",
+    "postalCode": "81-602",
+    "addressCountry": "PL"
+  },
+  "contactPoint": {
+    "@type": "ContactPoint",
+    "telephone": "+48 669 256 146",
+    "contactType": "Mobile"
+  }
+}
+ </script>
+
       </head>
       <body
         className={`${raleway.className} text-dark dark:text-secondary bg-[#FAFAFA] dark:bg-dark`}
